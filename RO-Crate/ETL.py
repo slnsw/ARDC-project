0cf95adc20807eaefb2586a99fba5f2f93a96b4f
Salek Ali <muhammadsalek.ali@studio.unibo.it>
2019-10-03 13:30:56 +1000

    Update ETL.py

--- RO-Crate/ETL.py
+++ RO-Crate/ETL.py
@@ -16,8 +16,8 @@ def main():
     api_pds_endpoint = 'https://libprd70.sl.nsw.gov.au/pds'
     api_sru_endpoint = 'http://digital.sl.nsw.gov.au/search/permanent/sru'
     
-    api_username = 'xxxxxxx'
-    api_password = 'xxxxxxxxxx'
+    api_username = 'api_etl'
+    api_password = 'blahBlah56'
     api_institude_code = 'SLNSW'
         
     ros = Rosetta(api_endpoint, api_pds_endpoint, api_sru_endpoint, api_username, api_password, api_institude_code, api_timeout=1200)
@@ -49,22 +49,51 @@ def main():
     mmsid = []
     
     for index, IE_PID in enumerate(IE_PIDs):
-        print("******************** Book ",index, " - IE PID: ", IE_PID, " ******************** \n")
+        print("\n ******************** Book ",index, " - IE PID: ", IE_PID, " ******************** \n")
         obj_x,mmsid_x = glean(IE_PID, ros)   
         objectfiles.append(obj_x)
         mmsid.append(mmsid_x)
         
+    savebooksinfo(IE_PIDs,mmsid,objectfiles)
+        
     for index, IE_PID in enumerate(IE_PIDs):
-        print("******************** Creating JSONLD for Book ",index, " ******************** \n")
+        print("\n ******************** Creating JSONLD for Book ",index, " ******************** \n")
         roCrateJsonld(IE_PID, objectfiles[index], mmsid[index])
         
     for index, IE_PID in enumerate(IE_PIDs):
         print("\n ******************** Retrieving files for Book ",index, " ******************** \n")
-        getFiles(IE_PID, objectfiles[index])        
-    
-        
+        getFiles(IE_PID, objectfiles[index])
 
-       
+
+
+
+def savebooksinfo(IE_PIDs,mmsid,objectfiles):
+    # Saving 100 books IE PID, MMSID and Objectfiles to external csv (optional)
+    booksinfo = pd.DataFrame(
+            {'IE PIDs': IE_PIDs,
+             'MMSIDs': mmsid,
+             'OBJ FILES': objectfiles
+             })
+    booksinfo.head()
+    booksinfo.to_csv("booksinfo.csv", sep='\t', index=False, encoding='utf-8')
+    
+    
+    
+def restorebooksinfo():
+    # Restoring booksinfo (optional)
+    bookdf = pd.read_csv('booksinfo.csv', sep='\t')
+    IE_PIDs1 = list(bookdf["IE PIDs"])
+    MMSID1 = [] 
+    for i in range(100):
+        MMSID1.append(eval(bookdf["MMSIDs"][i]))
+    OBJFILES1 = []
+    for i in range(100):
+        OBJFILES1.append(eval(bookdf["OBJ FILES"][i]))
+    return IE_PIDs1,MMSID1,OBJFILES1
+        
+    
+
+
     
 if __name__ == '__main__':
     main()
